4a6c70a44ccf4269c5b4b29c45081da8f0b5d903
Suvra ghosh <103570957+suvraghosh@users.noreply.github.com>
2023-08-23 21:41:14 +0530

    Embedded JavaScript

---
+++ Section25- EJS/4.2 Passing Data/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { name } from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+});
+
+app.post("/submit", (req, res) => {
+  var fullName = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs",{
+    numberOfLetters : fullName,
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
